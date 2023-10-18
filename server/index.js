25c13fff138d38ac7746b7a13a55cd76c674b69b
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-10-18 19:33:55 +0200

    Add visual messages

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('an user has disconnected')
   })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
 })
 
 app.use(logger('dev'))
